924b98b71e570ac9f660a581abc5d8655f86cc89
dash <feipyang@gmail.com>
2022-02-20 10:15:59 +0800

    First commitment

---
+++ .built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/android/built-in.o :=   rm -f drivers/android/built-in.o; /root/Code/android10_redroid/prebuilts/qemu-kernel/kernel-toolchain/android-kernel-toolchain-ar rcSTPD drivers/android/built-in.o drivers/android/binderfs.o drivers/android/idr.o drivers/android/binder.o drivers/android/binder_alloc.o 
